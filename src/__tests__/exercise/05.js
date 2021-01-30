27f27d2f61ec24e10f25ed7e4e16398de2a2400b
Kate <test@gmail.com>
2021-01-30 15:36:44 -0500

    test unkown error

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -13,11 +13,13 @@ import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login-submission'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
+import {rest} from 'msw'
 
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.restoreHandlers())
 
 const buildLoginForm = build({
   fields: {
@@ -52,4 +54,23 @@ test('logining in without password causes alert', async () => {
   expect(screen.getByRole(/alert/i).textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+// test unknow error
+test('unknown server error displays the error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
